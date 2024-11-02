37c8ceb6154cd08d8019ca25b286c2c1b58f4c3e
MarcelHerbst <167522757+MarcelHerbst@users.noreply.github.com>
2024-11-02 14:53:44 +0100

    Class implements (finish)

--- Solar_System/Solar_System_Class.ts
+++ Solar_System/Solar_System_Class.ts
@@ -3,13 +3,13 @@ namespace Solar_System{
 export class Body {
     public planets: Body[];
     public size: number;
-    public speed: Vektor;
+    public speed: number;
     public pos: Vektor;
     public color: Vektor;
     public desc?: string; 
     public name?: string;
 
-    constructor(_planets: Body[], _size:number, _speed: Vektor, _pos: Vektor, _color: Vektor, _desc: string, _name: string){
+    constructor(_planets: Body[], _size:number, _speed: number, _pos: Vektor, _color: Vektor, _desc: string, _name: string){
    
     this.planets = [];
     this.size = _size;
@@ -21,13 +21,26 @@ export class Body {
 }
 }
 
-export class Planet{
-    
+export class Planet extends Body{
+    public hasRing : boolean;
+    public moons : Body[];
+
+    constructor(_planets: Body[], _size:number, _speed:number, _pos: Vektor, _color:Vektor, _desc: string, _name: string, _hasRing: boolean, _moons: Body[]){
+        super(_planets, _size, _speed, _pos, _color, _desc, _name)
+        this.hasRing = _hasRing;
+        this.moons = _moons;
+        
+    }
+
 }
 
+export class Moon extends Body{
+    public orbits: string;
 
-
-export class Moon{
+    constructor(_planets: Body[], _size: number, _speed: number, _pos: Vektor, _color: Vektor, _desc: string, _name: string, _orbits: string){
+        super(_planets, _size, _speed, _pos, _color, _desc, _name)
+        this.orbits = _orbits;
+    }
 
 }
 
